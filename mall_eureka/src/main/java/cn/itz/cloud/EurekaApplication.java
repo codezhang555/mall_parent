7584ad45b0389dc67ea30bee6cd70d54d8a94961
zhangcompany <codezhangzzz@163.com>
2021-01-11 16:15:22 +0800

    添加Eureka微服务模块

---
+++ mall_eureka/src/main/java/cn/itz/cloud/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.itz.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @PackageName: cn.itz.cloud
+ * @ClassName: EurekaApplication
+ * @Author: codeZhang
+ * @DateTime: 2021/1/11 14:38
+ * @Version 1.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class);
+  }
+}
